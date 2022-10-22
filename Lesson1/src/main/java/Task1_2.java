881234cfefdc10835779d1a3ef8541f9813d5b96
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-10-22 15:06:36 -0400

    решение домашнего задания 1.2 часть с компилятором terminal

--- Lesson1/src/main/java/Task1_2.java
+++ Lesson1/src/main/java/Task1_2.java
@@ -1,13 +1,7 @@
 public class Task1_2 {
     public static void main(String[] args) {
         int count = 0;
-        String[] strings = new String[5];
-        strings[0] = "23";
-        strings[1] = "89";
-        strings[2] = "1";
-        strings[3] = "-1";
-        strings[4] = "3";
-        for (String arg : strings) {
+        for (String arg : args) {
             int i = Integer.parseInt(arg);
             if (i > 0) {
                 count++;
