80aba39c47193c1b946a8a1a424eb8ad496ca105
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-11-06 16:31:25 -0500

    Lesson3 updated Lesson4 Homework 3 exercises

--- Lesson3/src/main/java/countSum.java
+++ Lesson3/src/main/java/countSum.java
@@ -5,7 +5,7 @@ public class countSum {
     }
 
     public static int sum(int a, int b) {
-        if ((a + b) > Integer.MAX_VALUE) { //нужно ли написать Integer.max(a,b) ?? 
+        if ((a + b) > Integer.MAX_VALUE) { //нужно ли написать Integer.max(a,b) ??
             return -1;
         } else {
             return a + b;
