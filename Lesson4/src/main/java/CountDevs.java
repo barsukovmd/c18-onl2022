d04203a0bf0704d426dd39f5f05fde6c21cd9548
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-11-15 17:08:29 -0500

    Задача на колличество программистов переделана (update)

--- Lesson4/src/main/java/CountDevs.java
+++ Lesson4/src/main/java/CountDevs.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
-public class CountDevs { //нужно ли вам оставлять задание вверху, чтобы видели? или удалять?
+public class CountDevs {
     public static void main(String[] args) {
         System.out.println("Enter number of DevOps");
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         countNumberOfDevs(x);
+        for (int i = 0; i < 1000; i++) {
+            countNumberOfDevs(i);
+
+        }
     }
 
     public static void countNumberOfDevs(int x) {
         int d10 = x % 10;
         int d100 = x % 100;
-        if (d10 > 1 && d10 < 5 || d100 > 12) {
-            System.out.println(x + " программиста");
-        } else if (d10 > 4 || d10 == 0) {
-            System.out.println(x + " программистов");
-        } else {
+        if (d10 == 1 && d100 != 11) {
             System.out.println(x + " программист");
+        } else if ((d10 >= 2 && d10 <= 4) && !(d100 >= 12 && d100 < 14)) {
+            System.out.println(x + " программиста");
+        } else {
+            System.out.println(x + " программистов");
         }
     }
 }
