750f5880eb90848e40f5f3eeba5af2cd428f4827
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-11-15 16:52:32 -0500

    Задача на колличество программистов переделана

--- Lesson4/src/main/java/CountDevs.java
+++ Lesson4/src/main/java/CountDevs.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
-public class CountDevs {
+public class CountDevs { //нужно ли вам оставлять задание вверху, чтобы видели? или удалять?
     public static void main(String[] args) {
         System.out.println("Enter number of DevOps");
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        getCountDevs(x);
+        countNumberOfDevs(x);
     }
 
-    public static void getCountDevs(int x) {
-        if (x < 5 & x % 2 == 0 & x % 3 == 0) {
+    public static void countNumberOfDevs(int x) {
+        int d10 = x % 10;
+        int d100 = x % 100;
+        if (d10 > 1 && d10 < 5 || d100 > 12) {
             System.out.println(x + " программиста");
-        } else if (x >= 5 | x % 2 == 0 | x % 3 == 0 | x % 5 == 0) {
+        } else if (d10 > 4 || d10 == 0) {
             System.out.println(x + " программистов");
         } else {
             System.out.println(x + " программист");
@@ -22,13 +24,5 @@ public class CountDevs {
 
 
 
-/*
- * На вход приходит число.
- * Вывести в консоль фразу из разряда "_COUNT_ программистов",
- * заменить _COUNT_ на число которое пришло на вход в метод и заменить окончание в слове "программистов" на
- * уместное с точки зрения русского языка.
- * Пример: 1 программист, 42 программиста, 50 программистов
- *
- * @param count - количество программистов
- */
+
 
