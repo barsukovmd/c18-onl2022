e066234128ab00b8f4a9b1889de9fd78d6829ca2
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-11-27 15:14:58 -0500

    Вроде не сложно, но есть почему-то вопросы

--- Lesson8/src/main/java/car/FuelTank.java
+++ Lesson8/src/main/java/car/FuelTank.java
@@ -1,13 +1,17 @@
 package car;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class FuelTank {
-    private int fuel = 50;
+    private int fuel;
+
     public FuelTank(int fuel) {
         this.fuel = fuel;
     }
+
     public void checkFuel(int fuel) {
         for (int i = 0; i < fuel; i++) {
             fuel--;
