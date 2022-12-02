d0e51f1439a1be7bad66ddde54c916f3e35c4160
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-12-01 22:09:48 -0500

    Lesson 9 Homework, part 2

--- Lesson9/src/main/java/homework9Task2/Bouquet.java
+++ Lesson9/src/main/java/homework9Task2/Bouquet.java
@@ -7,14 +7,15 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Bouquet {
+public class Bouquet extends Flower {
     Flower[] flowers;
 
-    public Bouquet(Flower[] flowers) {
+    public Bouquet(String name, int price, Flower[] flowers) {
+        super(name, price);
         this.flowers = flowers;
     }
 
-    public int getPrice(FlowerType flowerType) {
+    public int getPrice() {
         int price = 0;
         for (Flower allFlowers : flowers) {
             price += allFlowers.getPrice();
