a50d4ee78cd6d92bdd4056340bc089396f178692
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-12-07 00:51:35 -0500

    updated

--- Lesson11/src/main/java/by/tms/test/Main.java
+++ Lesson11/src/main/java/by/tms/test/Main.java
@@ -2,13 +2,14 @@ package by.tms.test;
 
 public class Main {
     public static void main(String[] args) {
+        Book book = new Book("Lords of the Rings", new Author("Tolkien"), 1954);
+        System.out.println(book);
         try {
-            Book book = new Book("Lords of the Rings", new Author("Tolkien"), 1983);
-            Book book1 = book.clone();
-            book1.setName("Harry Potter");
-            book1.setAuthor(new Author("J.K.Rowlings"));
-            System.out.println(book);
-            System.out.println(book1);
+            Book bookClone = book.clone();
+            bookClone.setName("Harry Potter");
+            bookClone.setAuthor(new Author("J.K.Rowlings"));
+            bookClone.setYear(1997);
+            System.out.println(bookClone);
         } catch (CloneNotSupportedException exception) {
             System.out.println("Cloning not supported");
         }
