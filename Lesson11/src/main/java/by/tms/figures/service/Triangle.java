7e0245d6372bca03a097469e618ef8e732a5cf5c
Yauhen Barsukov <tenniszh2009@gmail.com>
2022-12-23 15:13:29 -0500

    updated homework

--- Lesson11/src/main/java/by/tms/figures/service/Triangle.java
+++ Lesson11/src/main/java/by/tms/figures/service/Triangle.java
@@ -3,6 +3,10 @@ package by.tms.figures.service;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+//почему-то не видит мой superbuilder ошибка
+//java: cannot find symbol
+//  symbol:   class FigureBuilder
+//  location: class by.tms.figures.service.Figure
 public class Triangle extends Figure implements IFigure {
     private double height;
     private double base;
