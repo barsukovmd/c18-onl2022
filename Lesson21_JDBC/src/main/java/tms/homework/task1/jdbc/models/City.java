bb617f379a14995b52364788c50b28e2bce13085
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-01-22 20:25:19 -0500

    homework jdbc

--- Lesson21_JDBC/src/main/java/tms/homework/task1/jdbc/models/City.java
+++ Lesson21_JDBC/src/main/java/tms/homework/task1/jdbc/models/City.java
@@ -1,27 +1,15 @@
 package tms.homework.task1.jdbc.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor
 public class City {
     private String cityName;
     private Student student;
-
-    public City(String cityName, Student student) {
-        this.cityName = cityName;
-        this.student = student;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
 }
--- Lesson21_JDBC/src/main/java/tms/homework/task1/jdbc/models/Student.java
+++ Lesson21_JDBC/src/main/java/tms/homework/task1/jdbc/models/Student.java
@@ -1,5 +1,14 @@
 package tms.homework.task1.jdbc.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor
 public class Student {
     private int id;
     private String name;
@@ -7,62 +16,4 @@ public class Student {
     private int age;
     private int course;
 
-    public Student(int id, String name, String surname, int age, int course) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.course = course;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getCourse() {
-        return course;
-    }
-
-    public void setCourse(int course) {
-        this.course = course;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
-                ", course=" + course +
-                '}';
-    }
 }
