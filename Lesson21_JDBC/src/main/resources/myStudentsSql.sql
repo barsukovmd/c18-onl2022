c72c6ba709be53df2f05c851a9ea96008e8940a0
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-01-30 00:37:34 -0500

    servlet homework +sql script

--- Lesson21_JDBC/src/main/resources/myStudentsSql.sql
+++ Lesson21_JDBC/src/main/resources/myStudentsSql.sql
@@ -1,5 +1,4 @@
-SELECT *
-FROM students_db.students
-         left join students_db.students on students_db.students.city_id = students_db.city.city_id;
-
+SELECT students_db.students.id, name, surname, age, course, city_id
+from students_db.students
+         left join students_db.city c on students.city_id = c.city_cityid;
 
