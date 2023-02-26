9b45f7ee5bc9b8aed618b9dbeb85bbde2df64edc
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-02-26 18:20:12 -0500

    update

--- TestCore/src/main/java/One/TestList.java
+++ TestCore/src/main/java/One/TestList.java
@@ -1,13 +1,15 @@
 package One;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.*;
 
 public class TestList {
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
-        strings.add("dgfaffgsq");
-        strings.add("khhhb");
-        strings.add("sdcaafd");
+        strings.add("udhbfvvvvvvvvvvvvv");//переход значения "слово" -> к значению буква
+        strings.add("khhhb");//переход значения "слово" -> к значению буква
+        strings.add("sdcaafd");//переход значения "слово" -> к значению буква
         strings.stream()
                 .map(s -> Arrays.stream(s.split("")).distinct().count())
                 .forEach(System.out::println);
