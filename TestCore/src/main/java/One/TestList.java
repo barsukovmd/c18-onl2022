05a3de114f88d31767b74423bf5fe9ac465f0315
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-02-26 18:21:22 -0500

    update

--- TestCore/src/main/java/One/TestList.java
+++ TestCore/src/main/java/One/TestList.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class TestList {
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
-        strings.add("udhbfvvvvvvvvvvvvv");//переход значения "слово" -> к значению буква
+        strings.add("udhbfvvvvvvvvvvvvv.");//переход значения "слово" -> к значению буква
         strings.add("khhhb");//переход значения "слово" -> к значению буква
         strings.add("sdcaafd");//переход значения "слово" -> к значению буква
         strings.stream()
