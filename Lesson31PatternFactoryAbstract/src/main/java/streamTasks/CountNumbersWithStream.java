293fef46a2c9699d6e71e1b8f904e75e8a4463a7
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-03-03 00:32:05 -0500

    hm 31

--- Lesson31PatternFactoryAbstract/src/main/java/streamTasks/CountNumbersWithStream.java
+++ Lesson31PatternFactoryAbstract/src/main/java/streamTasks/CountNumbersWithStream.java
@@ -1,13 +1,7 @@
 package streamTasks;
-
-import com.oracle.wls.shaded.org.apache.xalan.transformer.Counter;
-
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 //// Сделать вывод в формате: число из списка + "-Hello" и то,
 // сколько раз это число встречалось в списке, при этом не делать вывод для 4 и 6
@@ -15,9 +9,16 @@ import java.util.stream.IntStream;
 public class CountNumbersWithStream {
     public static void main(String[] args) {
         List<Integer> integers = List.of(1, 2, 5, 4, 4, 5, 2, 3, 6, 5);
-        System.out.printf(integers.stream()
-                .filter(integer -> integer != 4)
-                .filter(integer -> integer != 6)
-                .count() + "-Hello");
+        integers.stream()
+                .collect(Collectors.groupingBy(integer -> integer, Collectors.counting()))
+                .entrySet().stream().filter(i -> i.getValue() != 4 || i.getValue() != 6)
+                .forEach(count -> System.out.println(count.getKey() + ":" + count.getValue()));
+//        System.out.println(integers.stream()
+//                .filter(integer -> integer != 4)
+//                .filter(integer -> integer != 6)
+//                .collect(Collections.frequency(integers,)!=4||6)
+//                .count() + "-Hello");
+//        integers.forEach(System.out::print);
+
     }
 }
