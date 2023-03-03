7fb64943645e26eb65ecc79aad5cd37ceffaf2cb
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-03-03 00:36:34 -0500

    hm 31

--- Lesson31PatternFactoryAbstract/src/main/java/streamTasks/CountNumbersWithStream.java
+++ Lesson31PatternFactoryAbstract/src/main/java/streamTasks/CountNumbersWithStream.java
@@ -10,15 +10,16 @@ public class CountNumbersWithStream {
     public static void main(String[] args) {
         List<Integer> integers = List.of(1, 2, 5, 4, 4, 5, 2, 3, 6, 5);
         integers.stream()
+                .filter(integer -> integer != 4)
+                .filter(integer -> integer != 6)
                 .collect(Collectors.groupingBy(integer -> integer, Collectors.counting()))
-                .entrySet().stream().filter(i -> i.getValue() != 4 || i.getValue() != 6)
-                .forEach(count -> System.out.println(count.getKey() + ":" + count.getValue()));
+                .entrySet().stream().filter(i -> i.getValue() > 0)
+                .forEach(count -> System.out.println(count.getKey() + ":" + count.getValue() + " -Hello"));
 //        System.out.println(integers.stream()
 //                .filter(integer -> integer != 4)
 //                .filter(integer -> integer != 6)
 //                .collect(Collections.frequency(integers,)!=4||6)
 //                .count() + "-Hello");
 //        integers.forEach(System.out::print);
-
     }
 }
