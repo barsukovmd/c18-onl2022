d374c19bd708f0e0d16a25b791cbaea371b7976d
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-04-09 16:00:22 -0400

    updated

--- Lesson37Spring/src/main/java/Application.java
+++ Lesson37Spring/src/main/java/Application.java
@@ -10,8 +10,8 @@ public enum Application {
         if (dataSource == null) {
             MysqlDataSource dataSource = new MysqlDataSource();
             dataSource.setUser("root");
-            dataSource.setPassword("s3cr3t");
-            dataSource.setURL("jdbc:mysql://localhost:3306/myDatabase");
+            dataSource.setPassword("Barsukov28071992");
+            dataSource.setURL("jdbc:postgresql://localhost:5432/postgres");
             this.dataSource = dataSource;
         }
         return dataSource;
