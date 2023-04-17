cfa76531b0476839c7418612d795f48ecfd23c70
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-04-16 23:13:34 -0400

    updated

--- CalculatorWithArabic/src/main/java/calculator/CalculatorMain.java
+++ CalculatorWithArabic/src/main/java/calculator/CalculatorMain.java
@@ -4,7 +4,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorMain {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number1;
