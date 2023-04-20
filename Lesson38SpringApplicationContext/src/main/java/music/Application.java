767427d48875e0ed829372110ae156ec2d9c9ff4
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-04-20 14:34:59 -0400

    spring update

--- Lesson38SpringApplicationContext/src/main/java/music/Application.java
+++ Lesson38SpringApplicationContext/src/main/java/music/Application.java
@@ -5,19 +5,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Application {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+        context.close();
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer1.playMusic();
-        musicPlayer2.playMusic();
-        boolean comparison = musicPlayer1 == musicPlayer2;
-        musicPlayer1.setVolume(10);
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
-        System.out.println(musicPlayer1);
-        System.out.println(musicPlayer2);
-        System.out.println(comparison);
-        System.out.println("--------------------");
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer1.playMusic();
+//        musicPlayer2.playMusic();
+//        boolean comparison = musicPlayer1 == musicPlayer2;
+//        musicPlayer1.setVolume(10);
+//        System.out.println(musicPlayer1.getVolume());
+//        System.out.println(musicPlayer2.getVolume());
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
+//        System.out.println(comparison);
+//        System.out.println("--------------------");
     }
 }
--- Lesson38SpringApplicationContext/src/main/java/music/ClassicalMusic.java
+++ Lesson38SpringApplicationContext/src/main/java/music/ClassicalMusic.java
@@ -1,13 +1,32 @@
 package music;
 
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 @Setter
 public class ClassicalMusic implements Music {
+
+    private String config;
+
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {//при singleton создается только 1 bean объекта фабричным методом
+        return new ClassicalMusic();
+    }
 
     @Override
     public String getSong() {
         return "Beethoven symphony";
+    }
+
+    private void doMyInit() {
+        config = "doing my initialization";
+    }
+
+    private void doMyDestroy() {
+        config = "destroying my bean";
     }
 
 }
