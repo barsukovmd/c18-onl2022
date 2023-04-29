f55e8327e53a44ad86728c6c4445fbf0d21343fb
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-04-29 19:41:29 -0400

    spring update

--- Lesson40SpringMvcTest/src/main/java/us/controllers/FirstController.java
+++ Lesson40SpringMvcTest/src/main/java/us/controllers/FirstController.java
@@ -1,13 +1,26 @@
 package us.controllers;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        //Работаем с пришедшим от пользователя параметром
+//        return "first/hello";
+//    }
+
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = true) String surname) {
         return "first/hello";
     }
 
