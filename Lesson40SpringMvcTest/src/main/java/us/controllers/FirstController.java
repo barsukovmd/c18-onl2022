0c43674dea4556affab7af1b6bfb0526ce2f1044
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-04-29 20:30:49 -0400

    spring update

--- Lesson40SpringMvcTest/src/main/java/us/controllers/FirstController.java
+++ Lesson40SpringMvcTest/src/main/java/us/controllers/FirstController.java
@@ -1,8 +1,7 @@
 package us.controllers;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,16 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/first")
 public class FirstController {
 
-//    @GetMapping("/hello")
+    //    @GetMapping("/hello")
 //    public String helloPage(HttpServletRequest request) {
 //        String name = request.getParameter("name");
 //        //Работаем с пришедшим от пользователя параметром
 //        return "first/hello";
 //    }
-
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = true) String surname) {
+                            @RequestParam(value = "surname", required = true) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
@@ -28,5 +28,19 @@ public class FirstController {
     public String goodbyePage() {
         return "first/goodbye";
 
+    }
+
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model) {
+        double result = 0;
+        switch (action) {
+            case "multiplication" -> result = a * b;
+            case "division" -> result = (double) a / b;
+            case "summation" -> result = a + b;
+            case "subtraction" -> result = a - b;
+            default -> throw new RuntimeException();
+        }
+        model.addAttribute("result", result);
+        return "";
     }
 }
